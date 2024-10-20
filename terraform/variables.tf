4c2888cff4060bc47c4e9dadccfbd27e0f49f528
Chinenye Nwagbara <chinenye.nw@gmail.com>
2024-10-20 08:52:22 -0500

    Created variables.tf

---
+++ terraform/variables.tf
@@ -0,0 +1,24 @@
+variable vpc_cidr_block {
+  default = "10.0.0.0/16"
+}
+variable subnet_cidr_block {
+  default = "10.0.10.0/24"
+}
+variable avail_zone {
+  default = "eu-central-1a"
+}
+variable env_prefix {
+  default = "dev"
+}
+variable my_ip {
+  default = "85.246.32.98/32"
+}
+variable jenkins_ip {
+  default = "165.232.114.245/32"
+}
+variable instance_type {
+  default = "t2.micro"
+}
+variable region {
+  default = "eu-central-1"
+}
